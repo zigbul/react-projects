6c3e3615940585785069aa24e1f7eac70d7aa14b
zigbul <65498093+zigbul@users.noreply.github.com>
2024-11-09 23:29:20 +0300

    base calculator

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,5 +1,56 @@
+import { useState } from 'react';
+import './App.css';
+
 const App = () => {
-  return <div></div>;
+  const [inputValue, setInputValue] = useState('');
+
+  const clear = () => setInputValue('');
+  const display = (value) => setInputValue(inputValue + value);
+  const calculate = () => setInputValue(eval(inputValue));
+
+  return (
+    <div className="container">
+      <form className="calculator">
+        <input type="text" className="value" value={inputValue} />
+
+        <span onClick={() => display('1')}>1</span>
+        <span onClick={() => display('2')}>2</span>
+        <span onClick={() => display('3')}>3</span>
+        <span className="operator" onClick={() => display('+')}>
+          +
+        </span>
+
+        <span onClick={() => display('4')}>4</span>
+        <span onClick={() => display('5')}>5</span>
+        <span onClick={() => display('6')}>6</span>
+        <span className="operator" onClick={() => display('-')}>
+          -
+        </span>
+
+        <span onClick={() => display('7')}>7</span>
+        <span onClick={() => display('8')}>8</span>
+        <span onClick={() => display('9')}>9</span>
+        <span className="operator" onClick={() => display('*')}>
+          *
+        </span>
+
+        <span className="clear" onClick={() => clear()}>
+          C
+        </span>
+        <span onClick={() => display('0')}>0</span>
+        <span className="operator" onClick={() => display('.')}>
+          .
+        </span>
+        <span className="operator" onClick={() => display('/')}>
+          /
+        </span>
+
+        <span className="equal" onClick={() => calculate()}>
+          =
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default App;
